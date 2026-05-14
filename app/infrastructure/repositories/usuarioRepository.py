0393313165f53076ea744c0296917a7dd3b55d7a
RodamuCO <danestielm@udistrital.edu.co>
2026-05-14 13:21:42 -0500

    fix del fix de correo

--- app/infrastructure/repositories/usuarioRepository.py
+++ app/infrastructure/repositories/usuarioRepository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, or_
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.infrastructure.models.usuario import Usuario
@@ -12,17 +12,22 @@ class UsuarioRepository:
         self.db = db
 
     def buscarPorCorreo(self, correo: str):
-        stmt = (
-        select(Usuario)
-        .join(Persona)
-        .outerjoin(Empleado, Empleado.id == Persona.id)
-        .where(
-            or_(
-                Persona.correo == correo,
-                Empleado.correoLaboral == correo
+
+        if correo.endswith("@cinepacho.com"):
+            stmt = (
+                select(Usuario)
+                .join(Persona)
+                .join(Empleado, Empleado.id == Persona.id)
+                .where(Empleado.correoLaboral == correo)
             )
-        )
-    )
+
+        else:
+            stmt = (
+                select(Usuario)
+                .join(Persona)
+                .where(Persona.correo == correo)
+            )
+
         result = self.db.execute(stmt)
         return result.scalar_one_or_none()
 
